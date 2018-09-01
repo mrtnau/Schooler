0f61a950f2377a62873a406f488a95cac5a3ff7e
mrtnau <maschd51@gmail.com>
2018-09-01 16:03:17 +0200

    intial commit

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Code & Design</title>
+</head>
+<body>
+  <h1 id="Name">
+  Hello World
+  </h1>
+  <h2>
+  Code
+  </h2>
+  <?php
+    $variable = "Danke Chiara!";
+    echo $variable;
+    ?>
+    <p>
+    
+</body>
+</html>
---
+++ Schooler.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel= "stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <Header>
+        <img src="Logo.png" id="logo">
+        </header>
+    <main>
+        <section id="Tag1">
+        <section id="Tag2"> 
+    </main>
+    <nav id="Menübar">
+        <img src="Kalender.png" id="Downsize">
+        <img src="Chat.png">
+        <img src="News.png">
+        <img src="Profil.png">
+    </nav>
+</body>
+</html>
